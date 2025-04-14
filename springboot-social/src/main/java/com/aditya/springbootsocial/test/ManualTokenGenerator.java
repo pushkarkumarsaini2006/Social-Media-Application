959feba72558777708286783c8b1e92c116fb744
Pushkar Kumar Saini <pushkarkumarsaini2006@gmail.com>
2025-04-14 22:54:32 +0530

    third commit

---
+++ springboot-social/src/main/java/com/aditya/springbootsocial/test/ManualTokenGenerator.java
@@ -0,0 +1,13 @@
+package com.aditya.springbootsocial.test;
+
+import com.aditya.springbootsocial.config.JwtProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+public class ManualTokenGenerator {
+    public static void main(String[] args) {
+        String email = "test@example.com";
+        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(email, null);
+        String token = JwtProvider.generateToken(auth);
+        System.out.println("Generated Token: " + token);
+    }
+}
